77bb8a4ccf9b1ccee510329ac37da5298ab2db09
zjl980823 <36884151+zjl980823@users.noreply.github.com>
2019-05-27 20:44:54 +0800

    Update path_plan.cpp
    增加航点最大个数

--- ROS_CODE/wpb_home_apps/src/path_plan.cpp
+++ ROS_CODE/wpb_home_apps/src/path_plan.cpp
@@ -67,6 +67,21 @@ static void Init_waypoints()
     arWaypoint.push_back("3");
     arWaypoint.push_back("4");
     arWaypoint.push_back("5");
+    arWaypoint.push_back("6");
+    arWaypoint.push_back("7");
+    arWaypoint.push_back("8");
+    arWaypoint.push_back("9");
+    arWaypoint.push_back("10");
+    arWaypoint.push_back("11");
+    arWaypoint.push_back("12");
+    arWaypoint.push_back("13");
+    arWaypoint.push_back("14");
+    arWaypoint.push_back("15");
+    arWaypoint.push_back("16");
+    arWaypoint.push_back("17");
+    arWaypoint.push_back("18");
+    arWaypoint.push_back("19");
+    arWaypoint.push_back("20");
 }
 
 static void Speak(string inStr)
